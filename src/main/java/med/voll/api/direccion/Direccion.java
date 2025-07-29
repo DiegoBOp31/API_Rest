847667b57b61e387e7be240a60ebd96c000d0cb0
Diego Rosales Rebollar <diegorila-31@hotmail.com>
2025-07-28 18:41:33 -0600

    Se crea la entidad de médicos con su direccion embebida, se renombra el record de direccion, se agregan configuraciones para acceder a la base de datos

--- src/main/java/med/voll/api/direccion/Direccion.java
+++ src/main/java/med/voll/api/direccion/Direccion.java
@@ -1,12 +1,21 @@
 package med.voll.api.direccion;
 
-public record Direccion(
-        String calle,
-        String numero,
-        String complemento,
-        String barrio,
-        String ciudad,
-        String codigo_postal,
-        String estado
-) {
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter//Crea todos los getters en tiempo de compilación
+@NoArgsConstructor//Crea un constructor sin argumentos
+@AllArgsConstructor//Crea un constructor con todos los argumentos
+
+@Embeddable
+public class Direccion {
+    private String calle;
+    private String numero;
+    private String complemento;
+    private String barrio;
+    private String ciudad;
+    private String codigo_postal;
+    private String estado;
 }
---
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -0,0 +1,28 @@
+package med.voll.api.medico;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.direccion.Direccion;
+
+@Getter//Crea todos los getters en tiempo de compilación
+@NoArgsConstructor//Crea un constructor sin argumentos
+@AllArgsConstructor//Crea un constructor con todos los argumentos
+@EqualsAndHashCode(of = "id")//El sistema identifica que dos objetos son iguales si tienen el mismo id
+
+@Table(name = "medicos")
+@Entity(name = "Medico")
+public class Medico {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String email;
+    private String documento;
+    @Enumerated(EnumType.STRING)
+    private Especialidad especialidad;
+    @Embedded
+    private Direccion direccion;
+}
