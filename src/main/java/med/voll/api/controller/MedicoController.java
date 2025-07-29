2cebaeb3f465b0349f6f37f7579eddfe663dc3e7
Diego Rosales Rebollar <diegorila-31@hotmail.com>
2025-07-28 20:28:02 -0600

    Creamos un archivo sql para crear la tabla en la base de datos

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -4,6 +4,7 @@ import med.voll.api.medico.DatosRegistroMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,9 @@ public class MedicoController {
 
     @Autowired
     private MedicoRepository repository;
+
+    //Transactional lo usamos porque vamos a estar registrando datos en una base de datos
+    @Transactional//Estamos usando el transactional de spring boot
     //Maneja peticiones HTTP POST para registrar un nuevo recurso (por ejemplo, un médico)
     @PostMapping
     //RequestBody Indica que los datos del cuerpo de la petición HTTP se deben mapear a este objeto Java
