3ce6f6761b825c6e945d3e9550edab3f798ed563
Diego Rosales Rebollar <diegorila-31@hotmail.com>
2025-08-07 21:30:44 -0600

    Arreglamos la consulta de obtener un médico de forma aleatoria, el error estaba en el tipo de dato booleano

--- src/main/java/med/voll/api/domain/consulta/ReservaDeConsultas.java
+++ src/main/java/med/voll/api/domain/consulta/ReservaDeConsultas.java
@@ -5,6 +5,7 @@ import med.voll.api.domain.medico.Medico;
 import med.voll.api.domain.medico.MedicoRepository;
 import med.voll.api.domain.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -37,12 +38,13 @@ public class ReservaDeConsultas {
     }
 
     private Medico elegirMedico(DatosReservaConsulta datos) {
-        if(datos.idMedico()!=null){
+        if(datos.idMedico() != null){
             return medicoRepository.getReferenceById(datos.idMedico());
         }
-        if(datos.especialidad()==null){
-            throw new ValidacionException("Es necesario elegir una especialidad cuando no se elije un médico");
+        if(datos.especialidad() == null){
+            throw new ValidacionException("Es necesario elegir una especialidad cuando no se elige un médico");
         }
-        return medicoRepository.elegirMedicoDisponibleEnLaFecha(datos.especialidad(), datos.fecha());
+
+        return medicoRepository.elegirMedicoAleatorioDisponibleEnLaFecha(datos.especialidad(), datos.fecha());
     }
 }
--- src/main/java/med/voll/api/domain/medico/MedicoRepository.java
+++ src/main/java/med/voll/api/domain/medico/MedicoRepository.java
@@ -16,15 +16,16 @@ public interface MedicoRepository extends JpaRepository<Medico, Long> {
     @Query("""
             select m from Medico m
             where
-            m.activo = 1
+            m.activo = true
             and
             m.especialidad = :especialidad
             and m.id not in(
                 select c.medico.id from Consulta c
-                where c.fecha = :fecha
+                where
+                c.fecha = :fecha
             )
             order by rand()
             limit 1
             """)
-    Medico elegirMedicoDisponibleEnLaFecha(Especialidad especialidad, @NotNull @Future LocalDateTime fecha);
+    Medico elegirMedicoAleatorioDisponibleEnLaFecha(Especialidad especialidad, LocalDateTime fecha);
 }
