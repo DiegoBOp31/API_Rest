a9b1b56aeb378500f79c1aafdeba1aed33a39318
Diego Rosales Rebollar <diegorila-31@hotmail.com>
2025-08-11 18:21:41 -0600

    Agregamos métodos para fácilitar los test de medico repository

--- src/test/java/med/voll/api/domain/medico/MedicoRepositoryTest.java
+++ src/test/java/med/voll/api/domain/medico/MedicoRepositoryTest.java
@@ -1,6 +1,10 @@
 package med.voll.api.domain.medico;
 
 import jakarta.persistence.EntityManager;
+import med.voll.api.domain.consulta.Consulta;
+import med.voll.api.domain.direccion.DatosDireccion;
+import med.voll.api.domain.paciente.DatosRegistroPaciente;
+import med.voll.api.domain.paciente.Paciente;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +14,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.temporal.TemporalAdjuster;
 import java.time.temporal.TemporalAdjusters;
 
@@ -46,14 +51,79 @@ class MedicoRepositoryTest {
      */
     @DisplayName("Deberia devolver null cuando el medico buscado existe pero no esta disponible en esa fecha")
     void elegirMedicoAleatorioDisponibleEnLaFechaEscenario1() {
+        //given o arrange
         /**
          * Esto es para forzar que siempre se cumpla la condición de que el test se hará en un día y una hora aceptada
          * por el sistema para intentar crear una cita (siempre en lunes a las 10 am)
          */
         var lunesSiguienteALas10 = LocalDate.now().with(TemporalAdjusters.next(DayOfWeek.MONDAY)).atTime(10,0);
-
+        var medico = registrarMedico("Medico1", "medico@gmail.com", "12345", Especialidad.CARDIOLOGIA);
+        var paciente = registrarPaciente("Paciente1", "paciente@gmail.com", "123789");
+        registrarConsulta(medico, paciente, lunesSiguienteALas10);
+        //when o act
         var medicoLibre = medicoRepository.elegirMedicoAleatorioDisponibleEnLaFecha(Especialidad.CARDIOLOGIA, lunesSiguienteALas10);
-
+        //then o assert
         assertThat(medicoLibre).isNull();
+    }
+
+    @Test
+    @DisplayName("Deberia devolver medico cuando el medico buscado esta disponible en esa fecha")
+    void elegirMedicoAleatorioDisponibleEnLaFechaEscenario2() {
+        //given o arrange
+        var lunesSiguienteALas10 = LocalDate.now().with(TemporalAdjusters.next(DayOfWeek.MONDAY)).atTime(10,0);
+        var medico = registrarMedico("Medico1", "medico@gmail.com", "12345", Especialidad.CARDIOLOGIA);
+        //when o act
+        var medicoLibre = medicoRepository.elegirMedicoAleatorioDisponibleEnLaFecha(Especialidad.CARDIOLOGIA, lunesSiguienteALas10);
+        //then o assert
+        assertThat(medicoLibre).isEqualTo(medico);
+    }
+
+    private void registrarConsulta(Medico medico, Paciente paciente, LocalDateTime fecha) {
+        em.persist(new Consulta(null, medico, paciente, fecha));
+    }
+
+    private Medico registrarMedico(String nombre, String email, String documento, Especialidad especialidad) {
+        var medico = new Medico(datosMedico(nombre, email, documento, especialidad));
+        em.persist(medico);
+        return medico;
+    }
+
+    private Paciente registrarPaciente(String nombre, String email, String documento) {
+        var paciente = new Paciente(datosPaciente(nombre, email, documento));
+        em.persist(paciente);
+        return paciente;
+    }
+
+    private DatosRegistroMedico datosMedico(String nombre, String email, String documento, Especialidad especialidad) {
+        return new DatosRegistroMedico(
+                nombre,
+                email,
+                "6145489789",
+                documento,
+                especialidad,
+                datosDireccion()
+        );
+    }
+
+    private DatosRegistroPaciente datosPaciente(String nombre, String email, String documento) {
+        return new DatosRegistroPaciente(
+                nombre,
+                email,
+                "1234564",
+                documento,
+                datosDireccion()
+        );
+    }
+
+    private DatosDireccion datosDireccion() {
+        return new DatosDireccion(
+                "calle x",
+                "distrito y",
+                "ciudad z",
+                "123",
+                "CDMX",
+                "12345",
+                "CDMX"
+        );
     }
 }
