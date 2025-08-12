127cc709669023782b7aa48f35be8547a1667ee4
Diego Rosales Rebollar <diegorila-31@hotmail.com>
2025-08-11 18:01:28 -0600

    Empezamos a construir el primer escenario de prueba

--- src/test/java/med/voll/api/domain/medico/MedicoRepositoryTest.java
+++ src/test/java/med/voll/api/domain/medico/MedicoRepositoryTest.java
@@ -1,10 +1,19 @@
 package med.voll.api.domain.medico;
 
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjuster;
+import java.time.temporal.TemporalAdjusters;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -24,8 +33,27 @@ import static org.junit.jupiter.api.Assertions.*;
 @ActiveProfiles("test")
 class MedicoRepositoryTest {
 
-    @Test
-    void elegirMedicoAleatorioDisponibleEnLaFecha() {
+    @Autowired
+    private MedicoRepository medicoRepository;
 
+    @Autowired
+    private EntityManager em;
+
+    @Test
+    /**
+     * Esta notación sirve para nombrar de mejor forma nuestros test, para poder diferenciarlos correctamente unos
+     * de otros y ser más explícitos de lo que hace cada uno.
+     */
+    @DisplayName("Deberia devolver null cuando el medico buscado existe pero no esta disponible en esa fecha")
+    void elegirMedicoAleatorioDisponibleEnLaFechaEscenario1() {
+        /**
+         * Esto es para forzar que siempre se cumpla la condición de que el test se hará en un día y una hora aceptada
+         * por el sistema para intentar crear una cita (siempre en lunes a las 10 am)
+         */
+        var lunesSiguienteALas10 = LocalDate.now().with(TemporalAdjusters.next(DayOfWeek.MONDAY)).atTime(10,0);
+
+        var medicoLibre = medicoRepository.elegirMedicoAleatorioDisponibleEnLaFecha(Especialidad.CARDIOLOGIA, lunesSiguienteALas10);
+
+        assertThat(medicoLibre).isNull();
     }
 }
